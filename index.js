55a71ceff2b1369d04340bb32de269754641adde
Rizraelle <rizraelleshinobu@gmail.com>
2024-03-12 20:24:27 +0100

    Changed role tagging

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ client.on('guildMemberAdd', async(member) => {
 })
 
 client.on('messageCreate', async (message) => {
-  if (!message?.author.bot && message.content.includes('@Лига Петучей')) {
+  if (!message?.author.bot && message.content.includes(`<@&1215354313325682759>`)) {
+    console.log(message)
     const exampleEmbed = {
       title: 'ПЕТУШКИ ОБЩИЙ СБОР',
       image: {
